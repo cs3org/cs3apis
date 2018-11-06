412da8538b6d686f433ba87fab98837ad33e19b6
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2018-11-06 08:10:28 +0100

    about: fix formatting

--- gen/grpc-gateway/cernbox/about/v1/about.pb.gw.go
+++ gen/grpc-gateway/cernbox/about/v1/about.pb.gw.go
@@ -37,6 +37,15 @@ func request_About_ListMembers_0(ctx context.Context, marshaler runtime.Marshale
 
 }
 
+func request_About_GetDocumentation_0(ctx context.Context, marshaler runtime.Marshaler, client AboutClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetDocumentationRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.GetDocumentation(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterAboutHandlerFromEndpoint is same as RegisterAboutHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAboutHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -95,13 +104,37 @@ func RegisterAboutHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_About_GetDocumentation_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_About_GetDocumentation_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_About_GetDocumentation_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_About_ListMembers_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "about", "members"}, ""))
+
+	pattern_About_GetDocumentation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "about", "documentation"}, ""))
 )
 
 var (
 	forward_About_ListMembers_0 = runtime.ForwardResponseMessage
+
+	forward_About_GetDocumentation_0 = runtime.ForwardResponseMessage
 )
