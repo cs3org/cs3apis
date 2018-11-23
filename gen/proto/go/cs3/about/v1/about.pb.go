5d329b7b97871042ad5efe0837aeed4e55091f67
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2018-11-23 17:48:28 +0100

    all: change go import path

--- gen/proto/go/cs3/about/v1/about.pb.go
+++ gen/proto/go/cs3/about/v1/about.pb.go
@@ -6,7 +6,7 @@ package aboutv1pb
 import (
 	context "context"
 	fmt "fmt"
-	rpc "github.com/cs3/cs3apis/gen/proto/go/cs3/rpc"
+	rpc "github.com/cernbox/cs3apis/gen/proto/go/cs3/rpc"
 	proto "github.com/golang/protobuf/proto"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
