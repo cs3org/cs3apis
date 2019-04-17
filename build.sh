afdae4302843f8004d0b5ec14c7dcf9ba281b70c
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2019-04-17 15:55:19 +0200

    misc: add travis ci

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex  # Exit on error; debugging enabled.
+set -o pipefail # Fail a pipe if any sub-command fails.
+
+pushd /home/travis
+
+PROTOBUF_VERSION=3.3.0
+PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+PROTOTOOL_VERSION=1.6.0
+
+wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
+unzip ${PROTOC_FILENAME}
+cp bin/protoc /usr/local/bin/protoc
+protoc --version
+
+
+curl -sSL \
+  https://github.com/uber/prototool/releases/download/v${PROTOTOOL_VERSION}/prototool-Linux-x86_64.tar.gz | \
+  tar -C /usr/local --strip-components 1 -xz
+
+popd
